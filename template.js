39095fee7ddc44ff00d04bc4a6feeed72d53da31
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-06-06 12:21:27 -0400

    Add files via upload

---
+++ template.js
@@ -0,0 +1,29 @@
+
+window.onload=main;
+function main(){
+    console.log("main function...");
+    var links=document.querySelectorAll("a");
+    var today=new Date();
+    var y=today.getFullYear();
+    var m=today.getMonth();  //0-11  +1
+    var d=today.getDate();
+    var end=format(y,m+1,d);    
+    var start=new Date(y,m,d-2);
+    var startFormatted=format(start.getFullYear(),start.getMonth()+1,start.getDate());
+    links.forEach(function(link){
+        var prev=link.getAttribute("href");
+        var v1=prev.replace(/%%END%%/g, end);
+        var v2=v1.replace(/%%START%%/g,startFormatted);
+        link.setAttribute("href",v2);
+        // console.log(v2);
+    });
+
+    function format(y,m,d){
+        var res=y+'-';
+        if(m<10) res+='0';
+        res+=m+'-';
+        if(d<10)res+='0';
+        res+=d;
+        return res;
+    };
+};
